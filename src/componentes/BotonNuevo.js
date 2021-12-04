2c920c22bffdf1305a346987c4ae87b12c1bf7bc
bonnesbaby14 <bonnesbaby14@gmail.com>
2021-12-04 00:33:26 -0600

    estilos del boton

--- src/componentes/BotonNuevo.js
+++ src/componentes/BotonNuevo.js
@@ -1,10 +1,36 @@
 import React from "react";
 
-const BotonNuevo=()=>{
+
+const style = {
+    boton: {
+        backgroundColor: "#61DAFA",
+        boxShadow: "0px 5px 25px rgba(97, 218, 250, 0.5)",
+        border: "none",
+        borderRadius: "50%",
+        cursor: "pointer",
+        fontSize: "50px",
+        position: "fixed",
+        bottom: "24px",
+        right: "24px",
+        fontWeight: "bold",
+        color: "#FAFAFA",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "64px",
+        width: "64px",
+        transform: "rotate(0)",
+        transition: ".3s ease",
+        ":hover": {
+            transform: "rotate(224deg)"
+        }
+    }
+}
+const BotonNuevo = () => {
 
     return (
 
-<h1>Soy un boton</h1>
+        <butttom style={style.boton}>+</butttom>
 
     );
 }
