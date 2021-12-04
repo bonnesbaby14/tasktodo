f6722747918c5860dcffb9fa9133c21c0632d6b6
bonnesbaby14 <bonnesbaby14@gmail.com>
2021-12-04 00:43:52 -0600

    estilos de la barra de bisqueda

--- src/componentes/Buscar.js
+++ src/componentes/Buscar.js
@@ -1,11 +1,29 @@
 import React from "react";
 
-const Buscar =()=>{
+
+const style = {
+    buscar: {
+        background: " #F9FBFC",
+        borderRadius: "2px",
+        border: "2px solid #202329",
+        margin: "0 24px",
+        height: "64px",
+        width: "calc(100% - 62px)",
+        fontSize: "24px",
+        textAlign: "center",
+        fontFamily: 'Montserrat',
+        fontWeight: 400,
+        color: " #1E1E1F",
+        boxShadow: "0px 5px 50px rgba(32, 35, 41, 0.25)",
+    }
+}
+
+const Buscar = () => {
 
 
 
     return (
-        <h1>holjioajaijaia</h1>
+        <input style={style.buscar} placeholder="Buscar..."></input>
     );
 }
 
