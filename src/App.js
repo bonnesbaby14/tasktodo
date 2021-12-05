00ee740e56cd831a8382d7ea4e31d43b1b786398
bonnesbaby14 <bonnesbaby14@gmail.com>
2021-12-04 19:50:00 -0600

    Crear un hook para el localStorage

--- src/App.js
+++ src/App.js
@@ -8,61 +8,79 @@ import Contador from "./componentes/Contador"
 import { useState } from "react";
 
 
+
+
+const useLocalStorage = (itemName, initialValue) => {
+
+  const localstorageItem = localStorage.getItem(itemName);
+  let parseItems;
+
+  if (!localstorageItem) {
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parseItems = [];
+
+
+  } else {
+    parseItems = JSON.parse(localstorageItem);
+  }
+
+  const [items, setItems] = useState(parseItems);
+
+  const saveItem = (newItem) => {
+
+    localStorage.setItem(itemName, JSON.stringify(newItem));
+    setItems(newItem)
+
+  }
+
+  return [
+    items, saveItem
+  ]
+}
+
+
+
+
+
 function App() {
 
-  const localstorageTask=localStorage.getItem("TASK_V1");
-  let parsedTask;
+  const [tasks, setTasks] = useLocalStorage("TASK_V1", []);
 
-  const saveTask=(newTask)=>{
-
-    localStorage.setItem("TASK_V1",JSON.stringify(newTask));
-    setTasks(newTask)
-
-  }
-
-  if(!localstorageTask){
-    localStorage.setItem("TASK_V1",JSON.stringify([]));
-    parsedTask=[];
-
-
-  }else{
-    parsedTask=JSON.parse(localstorageTask);
-  }
 
 
   const [searchValue, setSearchValue] = useState('');
-  const [tasks, setTasks] = useState(parsedTask);
 
 
-  const completeTask=tasks.filter(task=>task.status==true).length;
-  const totalTaskComplete=tasks.length;
 
-  var  taskSearched =[];
+  const completeTask = tasks.filter(task => task.status == true).length;
+  const totalTaskComplete = tasks.length;
+
+  var taskSearched = [];
 
 
-  const completeTasks=(text)=>{
-    const taskIndex=tasks.findIndex(task=>task.task==text);
-    const newTasks=[...tasks];
-    newTasks[taskIndex].status=true;
+  const completeTasks = (text) => {
+    const taskIndex = tasks.findIndex(task => task.task == text);
+    const newTasks = [...tasks];
+    newTasks[taskIndex].status = true;
 
-    saveTask(newTasks);
-
-  }
-  const deleteTask=(text)=>{
-    const taskIndex=tasks.findIndex(task=>task.task==text);
-    const newTasks=[...tasks];
-    newTasks.splice(taskIndex,1);
-
-    saveTask(newTasks);
+    setTasks(newTasks);
 
   }
+  const deleteTask = (text) => {
+    const taskIndex = tasks.findIndex(task => task.task == text);
+    const newTasks = [...tasks];
+    newTasks.splice(taskIndex, 1);
 
-  if(searchValue.length < 0){
-    taskSearched=tasks;
-  }else{
-    taskSearched=tasks.filter(task=>{
-      const taskSearch =task.task.toLocaleLowerCase();
-      const searchText=searchValue.toLocaleLowerCase();
+    setTasks(newTasks);
+
+  }
+
+  if (searchValue.length < 0) {
+    taskSearched = tasks;
+  } else {
+    taskSearched = tasks.filter(task => {
+      const taskSearch = task.task.toLocaleLowerCase();
+      const searchText = searchValue.toLocaleLowerCase();
       return taskSearch.includes(searchText);
     })
   }
@@ -73,11 +91,11 @@ function App() {
       <Buscar searchValue={searchValue} setSearchValue={setSearchValue} />
       <BotonNuevo />
       <List>
-      {
-        taskSearched.map(task=>(<Item key={task.task} task={task.task} status={task.status} onComplete={()=>completeTasks(task.task)} onDelete={()=>{
-          deleteTask(task.task)
-        }}/>))
-      }
+        {
+          taskSearched.map(task => (<Item key={task.task} task={task.task} status={task.status} onComplete={() => completeTasks(task.task)} onDelete={() => {
+            deleteTask(task.task)
+          }} />))
+        }
       </List>
 
       <Contador total={totalTaskComplete} complete={completeTask} />
